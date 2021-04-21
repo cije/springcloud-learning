ea83d1665bd4df544b2cb8b9faecd7a1d8801c98
cije <an37500@163.com>
2021-04-21 11:22:00 +0800

    Spring Cloud Oauth2入门

---
+++ cloud-parent/oauth2-server/src/main/java/com/ce/oauth2server/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.ce.oauth2server.controller;
+
+/**
+ * @author c__e
+ * @date 2021/4/21 10:50
+ */
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by macro on 2019/9/30.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+}
---
+++ cloud-parent/oauth2-server/src/main/java/com/ce/oauth2server/service/UserService.java
@@ -0,0 +1,50 @@
+package com.ce.oauth2server.service;
+
+/**
+ * @author c__e
+ * @date 2021/4/21 10:45
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by macro on 2019/9/30.
+ */
+@Service
+public class UserService implements UserDetailsService {
+    private List<User> userList;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initData() {
+        String password = passwordEncoder.encode("123456");
+        userList = new ArrayList<>();
+        userList.add(new User("cije", password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin")));
+        userList.add(new User("test1", password, AuthorityUtils.commaSeparatedStringToAuthorityList("client")));
+        userList.add(new User("test2", password, AuthorityUtils.commaSeparatedStringToAuthorityList("client")));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<User> findUserList = userList.stream().filter(user -> user.getUsername().equals(username)).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(findUserList)) {
+            return findUserList.get(0);
+        } else {
+            throw new UsernameNotFoundException("用户名或密码错误");
+        }
+    }
+}
